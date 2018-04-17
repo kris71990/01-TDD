b4acb5bf64fa8589e66051dbf4102281c9322d0e
Kris Sakarias <ksakarias@gmail.com>
2018-04-16 21:23:59 -0700

    wrote modules

--- lab-kris/test/greet.test.js
+++ lab-kris/test/greet.test.js
@@ -8,3 +8,22 @@ describe('greet.js', () => {
     expect(greet('world')).toEqual('hello world');
   });
 });
+
+describe('arithmetic.js', () => {
+  describe('arithmetic.add', () => {
+    test('valid arguments', () => {
+      expect(arithmetic.add).not.toBeUndefined();
+    });
+    test('non-number argument', () => {
+      expect(arithmetic.add).toBeUndefined();
+    });
+  });
+  describe('arithmetic.sub', () => {
+    test('valid arguments', () => {
+      expect(arithmetic.add).not.toBeUndefined();
+    });
+    test('non-number argument', () => {
+      expect(arithmetic.sub).not.toBeUndefined();
+    });
+  });
+});
